f62c98f84441fb0656a3a2c1e8ca36bff8ba7b25
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2017-05-06 23:24:59 +0100

    dev: changed output for previews

--- preview.js
+++ preview.js
@@ -4,9 +4,10 @@ const fs = require( "fs" );
 const minimist = require( "minimist" );
 const preset = require( "./lib/preset" );
 
+const target = "./lib/preview";
 function save( name, preset ) {
 
-    name = `./lib/preview.${ name }.json`;
+    name = `${ target }/${ name }.json`;
     preset = JSON.stringify( preset, null, 4 );
 
     fs.writeFileSync( name, preset );
@@ -15,6 +16,8 @@ function save( name, preset ) {
 
 const args = minimist( process.argv.slice( 2 ) );
 const options = preset.preview( args );
+
+if ( ! fs.existsSync( target ) ) fs.mkdirSync( target );
 
 save( "preset", options.preset );
 save( "babel", options.babel );
